17b90ab120d360b7a9bd3ab57a7de2e691a92561
Aryarana7605 <aryarana7605@gmail.com>
2023-12-22 23:25:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// password123
+
+var pass = prompt("Enter Your Valid Password");
+
+if(pass=="password123"){
+    alert("Welcome")
+}
+else{
+    alert("You have entered an invalid Password")
+};
+
+
+// Prompt the user to enter a numeric grade (0-100).
+// Display the corresponding letter grade according to the following scale:
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// 0-59: Fs
+
